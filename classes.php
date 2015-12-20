845107a7fadff47c6dbb5f9e090e45a3d761b34f
ivanneira <ien1983@gmail.com>
2015-12-20 04:39:54 -0300

    funcionando pdo y conexion a la base de datos como estoy usando xampp. la manera mas práctica es usando user root y pass en blanco

--- classes.php
+++ classes.php
@@ -10,9 +10,9 @@ include_once 'db_access.php';
 
 try {
     $mySqlPDO = new PDO('mysql:host=' . $mySqlHost . ';dbname=' . $mySqlDb, $mySqlUser, $mySqlPass);
-    echo "intenta conectar <br>";
+
 }catch (PDOException $e) {
-    echo "encuentra excepcion";
+
     echo $e->getMessage();
 
 }
@@ -22,32 +22,59 @@ class db_connection{
 
     function searchUser($user){
         //aca busqueda solamente el usuario en la DB
+        $query = '';
+        global $mySqlPDO;
+
+        $mySqlQ = $mySqlPDO->prepare($query);
+        $mySqlQ->execute();
+        $rows = $mySqlQ->fetchAll(PDO::FETCH_ASSOC);
 
 
     }
 
     function matchPass($user,$pass){
         //busca coincidencia en usuario y contraseña.
-        return "test";
+        $query = '';
+        global $mySqlPDO;
+
+        $mySqlQ = $mySqlPDO->prepare($query);
+        $mySqlQ->execute();
+        $rows = $mySqlQ->fetchAll(PDO::FETCH_ASSOC);
 
     }
 
     function addUser($user,$pass){
 
         //verificar que no exista el usuario
-        return "test";
+        $query = '';
+        global $mySqlPDO;
+
+        $mySqlQ = $mySqlPDO->prepare($query);
+        $mySqlQ->execute();
+        $rows = $mySqlQ->fetchAll(PDO::FETCH_ASSOC);
 
         //agregar a la base de datos
+        $query = '';
+        global $mySqlPDO;
+
+        $mySqlQ = $mySqlPDO->prepare($query);
+        $mySqlQ->execute();
+        $rows = $mySqlQ->fetchAll(PDO::FETCH_ASSOC);
     }
 
     function removeUser($user){
         //borrar usuario
+        $query = '';
+        global $mySqlPDO;
+
+        $mySqlQ = $mySqlPDO->prepare($query);
+        $mySqlQ->execute();
+        $rows = $mySqlQ->fetchAll(PDO::FETCH_ASSOC);
     }
 
     //funcion de prueba
     function test(){
         global $mySqlPDO;
-
 
         $mySqlQ = $mySqlPDO->prepare('SELECT * FROM User');
         $mySqlQ->execute();
