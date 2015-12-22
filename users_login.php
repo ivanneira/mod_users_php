d3854a0d5e117f93b3cc56d5f37f074440430b2e
ivanneira <ien1983@gmail.com>
2015-12-22 00:44:39 -0300

    se elimina un archivo, quedan clases en uno solo

--- users_login.php
+++ users_login.php
@@ -7,12 +7,20 @@
  */
 
 include_once 'classes_db_access.php';
-include_once 'classes_verifications.php';
 
 $e = "false"; //por las dudas
 
+$user = $_POST["user"];
+$pass = $_POST["pass"];
 
+if (isset($_POST)) {
 
+    $checkVariables = new checkInput();
+
+    if($checkVariables->check($user,$pass)=='true'){
+        $e = 'true';
+    }else $e = 'false';
+}else $e = 'false';
 
 //solicita acceso, envia token
 if ($e == 'false') {
@@ -25,6 +33,3 @@ if ($e == 'false') {
     echo 'ACCESO CORRECTO';
 
 }
-
-
-?>
